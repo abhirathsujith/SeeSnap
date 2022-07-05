fc47802009f567aa4c9541c40896ee80673ff906
AbhirathSP <abhirathsp2@gmail.com>
2022-07-05 09:13:31 +0530

    IMP

--- SeeSnap/SeeSnap/ContentView.swift
+++ SeeSnap/SeeSnap/ContentView.swift
@@ -6,17 +6,44 @@
 //
 
 import SwiftUI
-import CoreData
+
+
+struct Snap: Identifiable {
+    var id: String { title }
+    let title: String
+    let rating: Int
+    let imgString: String
+    var bgString: String?
+    
+    static let photos = [
+    Snap(title: "Sunset", rating: 5, imgString: "1"),
+    Snap(title: "Sunset", rating: 5, imgString: "2"),
+    Snap(title: "Sunset", rating: 5, imgString: "3"),
+    Snap(title: "Sunset", rating: 5, imgString: "4"),
+    Snap(title: "Sunset", rating: 5, imgString: "5"),
+    Snap(title: "Sunset", rating: 5, imgString: "6"),
+    Snap(title: "Sunset", rating: 5, imgString: "7"),
+    Snap(title: "Sunset", rating: 5, imgString: "8"),
+    Snap(title: "Sunset", rating: 5, imgString: "9"),
+    Snap(title: "Sunset", rating: 5, imgString: "10"),
+    ]
+}
 
 struct ContentView: View {
     
+    let snaps = Snap.photos
+    
     var body: some View {
         GeometryReader { reader in
+            let screenSize = reader.size
             ZStack {
                 ScrollView(.horizontal) {
                     HStack{
-                        ForEach(0..<10) { i in
-                            Rectangle()
+                        ForEach(snaps) { snap in
+                            Image(snap.imgString)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: screenSize.width, height: screenSize.height)
                         }
                     }
                 }
