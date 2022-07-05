2923b0df573135136dd0764440b3da5ea92e50ae
AbhirathSP <abhirathsp2@gmail.com>
2022-07-05 08:54:45 +0530

    A

--- SeeSnap/SeeSnap/ContentView.swift
+++ SeeSnap/SeeSnap/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View
+struct ContentView: View {
+    
+    var body: some View {
+        GeometryReader { reader in
+            ZStack {
+                ScrollView(.horizontal) {
+                    HStack{
+                        ForEach(0..<10) { i in
+                            Rectangle()
+                        }
+                    }
+                }
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.cyan)
+        }
+    }
+}
    
         
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-      
+        ContentView()
     }
 }
